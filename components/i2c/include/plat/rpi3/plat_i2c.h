3ac3c0b4bd536e513e53c20922276067a2b1b679
Florian Hauschild <florian.hauschild@hensoldt.net>
2022-05-20 17:26:17 +0200

    [REF] SEOS-1326 Moved standard platform header into own header file

--- components/i2c/include/plat/rpi3/plat_i2c.h
+++ components/i2c/include/plat/rpi3/plat_i2c.h
@@ -1,18 +1,4 @@
 
 #pragma once
 
-#include "if_i2c.h"
-
-#include <stdint.h>
-#include <stdbool.h>
-
-
-
-bool _i2c_init(void);
-
-I2C_Error_t _i2c_init_slave(int dev);
-
-I2C_Error_t _i2c_write(int dev, const uint8_t* buf, size_t len, size_t* written);
-
-
-I2C_Error_t _i2c_read(int dev, uint8_t *buf, size_t len, size_t *read);
+#include "plat_i2c_generic.h"
--- components/i2c/include/plat/sabre/plat_i2c.h
+++ components/i2c/include/plat/sabre/plat_i2c.h
@@ -1,20 +1,8 @@
 
 #pragma once
 
-#include "if_i2c.h"
+#include "plat_i2c_generic.h"
 
 #include "arch/arm/i2c.h"
 
-#include <stdint.h>
-#include <stdbool.h>
-
 #define MAX_I2C_SLAVES 20
-
-bool _i2c_init(void);
-
-I2C_Error_t _i2c_init_slave(int dev);
-
-I2C_Error_t _i2c_write(int dev, const uint8_t* buf, size_t len, size_t* written);
-
-
-I2C_Error_t _i2c_read(int dev, uint8_t *buf, size_t len, size_t *read);
